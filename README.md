7008535d6c9b2017e2bea0abe281af3d63e92b8e
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-03-12 15:01:32 +0100

    [add] basic slider logic with Vue and and [add] steps in readme

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-Vue Slider
-===
+# Vue Slider
+
 Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
 
 **Bonus**
+
 1. al click su una thumb, visualizzare in grande l’immagine corrispondente
 2. applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 3. quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
 4. al doppio click sullo slider cambia la direzione dell’autoplay
 
+## Svolgimento
+
+1. Predisporre ambiente per lavorare con Vue: importare script per Vue, inserire tutto il contenuto del body dell'html in un elemento con id="app", destrutturare Vue in JS per ottenere il metodo createApp, inserire in createApp tutto il JS su cui lavorare
+2. Inserire nel return di data() l'array di immagini (array di oggetti) e un counter
+3. Mostrare l'immagine in base al counter
+4. Al click su uovo/uccello incrementare/decrementare il counter ed inserire controlli per creare slider infinito
+5. Caricare tutte le immagini nelle thumbnails (v-for)
+6. Rendere attiva la thumbnail con indice uguale al counter
--- js/script.js
+++ js/script.js
@@ -1,149 +1,206 @@
-const carouselContainer = document.querySelector(".my-carousel-container");
-const imagesWrapper = document.querySelector(".my-carousel-images");
-const thumbnailsWrapper = document.querySelector(".my-thumbnails-wrapper");
-const btnPrev = document.querySelector(".my-previous");
-const btnNext = document.querySelector(".my-next");
-const btnAutoplay = document.getElementById("my-btn-autoplay");
-const btnDirection = document.getElementById("my-btn-direction");
+const { createApp } = Vue;
 
-let counterImages = 0;
-let isAutoplay = false;
-let autoplay;
-let directionNext = true;
+createApp({
+  data() {
+    return {
+      images: [
+        {
+          url: "http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg",
+          title: "Svezia",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+        },
 
-const images = [
-  {
-    url: "http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg",
-    title: "Svezia",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+        {
+          url: "https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg",
+          title: "Perù",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+        },
+
+        {
+          url: "https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c",
+          title: "Chile",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+        },
+        {
+          url: "https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg",
+          title: "Argentina",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+        },
+        {
+          url: "https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop",
+          title: "Colombia",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+        },
+      ],
+      counter: 0,
+    };
   },
 
-  {
-    url: "https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg",
-    title: "Perù",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+  methods: {
+    //Logica cambio img al click con incremento/decremento del counter e controlli per slider infinito
+    goNext(isNext) {
+      isNext ? this.counter++ : this.counter--;
+
+      if (this.counter < 0) {
+        this.counter = this.images.length - 1;
+      } else if (this.counter === this.images.length) {
+        this.counter = 0;
+      }
+    },
   },
+}).mount("#app");
 
-  {
-    url: "https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c",
-    title: "Chile",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
-  },
-  {
-    url: "https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg",
-    title: "Argentina",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
-  },
-  {
-    url: "https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop",
-    title: "Colombia",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
-  },
-];
+// const carouselContainer = document.querySelector(".my-carousel-container");
+// const imagesWrapper = document.querySelector(".my-carousel-images");
+// const thumbnailsWrapper = document.querySelector(".my-thumbnails-wrapper");
+// const btnPrev = document.querySelector(".my-previous");
+// const btnNext = document.querySelector(".my-next");
+// const btnAutoplay = document.getElementById("my-btn-autoplay");
+// const btnDirection = document.getElementById("my-btn-direction");
 
-//Eliminazione contenuto statico del carosello (immagine + thumbnails)
-imagesWrapper.innerHTML = "";
-thumbnailsWrapper.innerHTML = "";
+// let counterImages = 0;
+// let isAutoplay = false;
+// let autoplay;
+// let directionNext = true;
 
-//Popolamento dinamico del carosello (immagine + thumbnails)
-images.forEach((image) => {
-  imagesWrapper.innerHTML += createTemplateImages(image);
-  thumbnailsWrapper.innerHTML += createTemplateThumbnails(image);
-});
+// const images = [
+//   {
+//     url: "http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg",
+//     title: "Svezia",
+//     description:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+//   },
 
-//Inserimento degli elementi creati in collection
-const imagesCollection = document.querySelectorAll(".my-carousel-item");
-imagesCollection[counterImages].classList.add("active");
+//   {
+//     url: "https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg",
+//     title: "Perù",
+//     description:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+//   },
 
-const thumbnailsCollection = document.querySelectorAll(".my-thumbnail");
-thumbnailsCollection[counterImages].classList.add("active");
+//   {
+//     url: "https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c",
+//     title: "Chile",
+//     description:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+//   },
+//   {
+//     url: "https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg",
+//     title: "Argentina",
+//     description:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+//   },
+//   {
+//     url: "https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop",
+//     title: "Colombia",
+//     description:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.",
+//   },
+// ];
 
-//Inserimento logica cambio immagine e thumbnails al click
-btnNext.addEventListener("click", goNext);
-btnPrev.addEventListener("click", goPrev);
+// //Eliminazione contenuto statico del carosello (immagine + thumbnails)
+// imagesWrapper.innerHTML = "";
+// thumbnailsWrapper.innerHTML = "";
 
-//Inserimento logica cambio immagine e thumbnails al click su thumbnails
-thumbnailsCollection.forEach((thumb, i) => {
-  thumb.addEventListener("click", () => changeImg(i));
-});
+// //Popolamento dinamico del carosello (immagine + thumbnails)
+// images.forEach((image) => {
+//   imagesWrapper.innerHTML += createTemplateImages(image);
+//   thumbnailsWrapper.innerHTML += createTemplateThumbnails(image);
+// });
 
-//Inserimento logica cambio immagine e thumbnails con autoplay tramite start/stop
-btnAutoplay.innerHTML = "Start" + " " + "autoplay";
+// //Inserimento degli elementi creati in collection
+// const imagesCollection = document.querySelectorAll(".my-carousel-item");
+// imagesCollection[counterImages].classList.add("active");
 
-btnAutoplay.addEventListener("click", () => {
-  console.log("Autoplay prima: ", isAutoplay);
-  if (isAutoplay === false) {
-    autoplay = setInterval(directionAutoplay, 3000);
-    isAutoplay = true;
-    btnAutoplay.innerHTML = "Stop" + " " + "autoplay";
-  } else if (isAutoplay === true) {
-    clearInterval(autoplay);
-    isAutoplay = false;
-    btnAutoplay.innerHTML = "Start" + " " + "autoplay";
-  }
-  console.log("Autoplay dopo: ", isAutoplay);
-});
+// const thumbnailsCollection = document.querySelectorAll(".my-thumbnail");
+// thumbnailsCollection[counterImages].classList.add("active");
 
-//Inserimento logica cambio direzione autoplay
-btnDirection.addEventListener("click", () => (directionNext = !directionNext));
+// //Inserimento logica cambio immagine e thumbnails al click
+// btnNext.addEventListener("click", goNext);
+// btnPrev.addEventListener("click", goPrev);
 
-// --- FUNCTIONS --- //
-function createTemplateImages(imgElement) {
-  return `
-  <div class="my-carousel-item">
-    <img
-      class="img-fluid"
-      src="${imgElement.url}"
-      alt="${imgElement.title} picture"
-    />
-    <div class="item-description px-3">
-      <h2>${imgElement.title}</h2>
-      <p>${imgElement.description}</p>
-    </div>
-  </div>
-  `;
-}
+// //Inserimento logica cambio immagine e thumbnails al click su thumbnails
+// thumbnailsCollection.forEach((thumb, i) => {
+//   thumb.addEventListener("click", () => changeImg(i));
+// });
 
-function createTemplateThumbnails(thumbElement) {
-  return `
-  <div class="my-thumbnail">
-    <img
-      class="img-fluid"
-      src="${thumbElement.url}"
-      alt="Thumbnail of ${thumbElement.title} picture"
-    />
-  </div>
-  `;
-}
+// //Inserimento logica cambio immagine e thumbnails con autoplay tramite start/stop
+// btnAutoplay.innerHTML = "Start" + " " + "autoplay";
 
-function goNext() {
-  let indexImg = counterImages;
-  indexImg === images.length - 1 ? (indexImg = 0) : indexImg++;
-  changeImg(indexImg);
-}
+// btnAutoplay.addEventListener("click", () => {
+//   console.log("Autoplay prima: ", isAutoplay);
+//   if (isAutoplay === false) {
+//     autoplay = setInterval(directionAutoplay, 3000);
+//     isAutoplay = true;
+//     btnAutoplay.innerHTML = "Stop" + " " + "autoplay";
+//   } else if (isAutoplay === true) {
+//     clearInterval(autoplay);
+//     isAutoplay = false;
+//     btnAutoplay.innerHTML = "Start" + " " + "autoplay";
+//   }
+//   console.log("Autoplay dopo: ", isAutoplay);
+// });
 
-function goPrev() {
-  let indexImg = counterImages;
-  indexImg === 0 ? (indexImg = images.length - 1) : indexImg--;
-  changeImg(indexImg);
-}
+// //Inserimento logica cambio direzione autoplay
+// btnDirection.addEventListener("click", () => (directionNext = !directionNext));
 
-function changeActiveStatus(counter) {
-  imagesCollection[counter].classList.toggle("active");
-  thumbnailsCollection[counter].classList.toggle("active");
-}
+// // --- FUNCTIONS --- //
+// function createTemplateImages(imgElement) {
+//   return `
+//   <div class="my-carousel-item">
+//     <img
+//       class="img-fluid"
+//       src="${imgElement.url}"
+//       alt="${imgElement.title} picture"
+//     />
+//     <div class="item-description px-3">
+//       <h2>${imgElement.title}</h2>
+//       <p>${imgElement.description}</p>
+//     </div>
+//   </div>
+//   `;
+// }
 
-function directionAutoplay() {
-  directionNext ? goNext() : goPrev();
-}
+// function createTemplateThumbnails(thumbElement) {
+//   return `
+//   <div class="my-thumbnail">
+//     <img
+//       class="img-fluid"
+//       src="${thumbElement.url}"
+//       alt="Thumbnail of ${thumbElement.title} picture"
+//     />
+//   </div>
+//   `;
+// }
 
-function changeImg(index) {
-  changeActiveStatus(counterImages);
-  counterImages = index;
-  changeActiveStatus(counterImages);
-}
+// function goNext() {
+//   let indexImg = counterImages;
+//   indexImg === images.length - 1 ? (indexImg = 0) : indexImg++;
+//   changeImg(indexImg);
+// }
+
+// function goPrev() {
+//   let indexImg = counterImages;
+//   indexImg === 0 ? (indexImg = images.length - 1) : indexImg--;
+//   changeImg(indexImg);
+// }
+
+// function changeActiveStatus(counter) {
+//   imagesCollection[counter].classList.toggle("active");
+//   thumbnailsCollection[counter].classList.toggle("active");
+// }
+
+// function directionAutoplay() {
+//   directionNext ? goNext() : goPrev();
+// }
+
+// function changeImg(index) {
+//   changeActiveStatus(counterImages);
+//   counterImages = index;
+//   changeActiveStatus(counterImages);
+// }
